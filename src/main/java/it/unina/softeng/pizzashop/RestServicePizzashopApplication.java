dc201a1b9ba454efeba23676fa4979fcf2200d4c
Luigi Starace <luigi.starace@gmail.com>
2022-10-29 16:23:27 +0200

    initial commit

---
+++ src/main/java/it/unina/softeng/pizzashop/RestServicePizzashopApplication.java
@@ -0,0 +1,13 @@
+package it.unina.softeng.pizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicePizzashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicePizzashopApplication.class, args);
+	}
+
+}
